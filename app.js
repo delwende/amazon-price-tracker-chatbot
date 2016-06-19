b9f0c42e93844819c58e08b1a8f2ba1d47167ded
Simon Kofler <simon.kofler@hotmail.com>
2016-06-19 16:08:30 +0200

    Update basic set price alert functionality

--- app.js
+++ app.js
@@ -523,7 +523,7 @@ function receivedPostback(event) {
 
               product.set("asin", asin);
               product.set("title", title);
-              product.set("detailPageURL", detailPageURL);
+              product.set("detailPageUrl", detailPageURL);
               product.set("largeImageUrl", largeImageUrl);
 
               product.save(null, {
@@ -535,7 +535,7 @@ function receivedPostback(event) {
                   var priceAlert = new PriceAlert();
 
                   priceAlert.set("product", {__type: "Pointer", className: "Product", objectId: product.id});
-                  priceAlert.set("user", {__type: "Pointer", className: "User", objectId: parseUserObjectId});
+                  priceAlert.set("user", {__type: "Pointer", className: "_User", objectId: parseUserObjectId});
                   priceAlert.set("locale", parseUserLocale);
 
                   priceAlert.save(null, {

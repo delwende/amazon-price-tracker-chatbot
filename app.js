d44f4ce2fefa004016fce7076aa571f81fd34a05
simonkofler <simonkofler@users.noreply.github.com>
2016-06-21 15:45:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1028,7 +1028,7 @@ function callSendAPI(messageData) {
         messageId, recipientId);
     } else {
       console.error("Unable to send message.");
-      console.error(response);
+      // console.error(response);
       // console.error(error);
     }
   });  

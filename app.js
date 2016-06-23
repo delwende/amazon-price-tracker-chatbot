0bca1d3020c7e302bde5060acf446487665d07c5
Simon Kofler <VDS2088@uniqa.at>
2016-06-23 15:19:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -331,7 +331,7 @@ function receivedMessage(event) {
             default:
               if (messageText.startsWith("help")) {
                 sendTextMessage(senderID, "Hi {}. So I monitor millions of products on Amazon and can alert you when prices drop, helping you decide when to buy. Tell me things like the following:\n- \"search \[product name\]\", e.g. \"search iphone6\"\n- \"list\" to show your price watches");
-                sendTextMessage(senderID, 'Hi {parseUserFirstName}, type "help"!'.format(user));
+                sendTextMessage(senderID, 'Hi {}, type "help"!'.format(user.parseUserFirstName));
               } else if (messageText.startsWith("search ")) {
                 sendTextMessage(senderID, "");
               } else if (messageText.startsWith("list")) {

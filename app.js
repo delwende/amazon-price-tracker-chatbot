54c3d97fce571cdf8e175de59fd8ae40a7f4d62a
simonkofler <simonkofler@users.noreply.github.com>
2016-06-30 08:00:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -154,6 +154,22 @@ app.get('/webhook', function(req, res) {
   }
 });
 
+app.get('/deleteredisuser', function(req, res) {
+  client.del('user:979287485518975', function(err, reply) {
+    console.log(reply);
+  });
+});
+
+app.get('/changeredituserlocale/:locale', function(req, res) {
+  var locale = req.params.locale;
+  client.hmset('user:979287485518975', 'parseUserLocale', locale);
+});
+
+app.get('/changeredituserlanguage/:language', function(req, res) {
+  var language = req.params.language;
+  client.hmset('user:979287485518975', 'parseUserLanguage', locale);
+});
+
 /*
  * All callbacks for Messenger are POST-ed. They will be sent to the same
  * webhook. Be sure to subscribe your app to your page to receive callbacks

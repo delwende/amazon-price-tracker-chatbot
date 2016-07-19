8c21ddabd9f2f939c9c4b754f3b411b7af5c590a
Simon Kofler <simon.kofler@hotmail.com>
2016-07-19 19:30:10 +0200

    Integrate expressjs generator skeleton project

--- app.js
+++ app.js
@@ -27,14 +27,39 @@ const
   sprintf = require('sprintf-js').sprintf,
   vsprintf = require('sprintf-js').vsprintf,
   helpers = require(__dirname + '/libs/helpers'), // Custom mixed helper functions
-  moment = require('moment'); // Parse, validate, manipulate, and display dates
+  moment = require('moment'), // Parse, validate, manipulate, and display dates
+  path = require('path'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  routes = require('./routes/index'),
+  users = require('./routes/users');
 
+
+// var app = express();
+
+// app.set('port', process.env.PORT || 5000);
+// app.use(bodyParser.json({ verify: verifyRequestSignature }));
+// app.use(express.static('public'));
 
 var app = express();
 
 app.set('port', process.env.PORT || 5000);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
 
 /*
  * Be sure to setup your config values before running this code. You can
@@ -198,6 +223,37 @@ app.post('/webhook', function (req, res) {
     // successfully received the callback. Otherwise, the request will time out.
     res.sendStatus(200);
   }
+});
+
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handlers
+
+// Development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// Production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 /*

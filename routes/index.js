2b9196b1607b5ce37b5737b3b4f98e3d80c73242
simonkofler <simonkofler@users.noreply.github.com>
2016-07-20 08:02:31 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var i18n = require("i18n");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'JackTheBot - free Amazon price tracking chatbot.' });
+  var title = i18n.__('JackTheBot - free Amazon price tracking chatbot.');
+  res.render('index', { title: title });
 });
 
 module.exports = router;

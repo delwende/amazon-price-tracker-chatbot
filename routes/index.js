8117d9406c3d822ab497c9e6399844e27e281be9
simonkofler <simonkofler@users.noreply.github.com>
2016-07-20 08:44:32 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ var i18n = require("i18n");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    titleAndDescription: i18n.__('JackTheBot - free Amazon price tracking chatbot.'),
-    title: i18n.__('JackTheBot'),
-    subtitle: i18n.__('The World\'s First Amazon Price Tracking Chatbot')
+    titleAndDescription: i18n.__("JackTheBot - free Amazon price tracking chatbot."),
+    title: i18n.__("JackTheBot"),
+    subtitle: i18n.__("The World's First Amazon Price Tracking Chatbot")
   });
 });
 

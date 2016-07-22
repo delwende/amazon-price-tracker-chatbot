806aa7efc098962d2fd87a5a69049a8337b25782
simonkofler <simonkofler@users.noreply.github.com>
2016-07-22 15:42:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1028,7 +1028,7 @@ function receivedPostback(event) {
                   var responseText = vsprintf(text, [productGroup, category, manufacturer, model, locale, ean, upc, sku, salesRank, salesRank,
                     salesRank, salesRank]);
 
-                  var responseTexts = snippets.chunk(responseText, { len: 300, breakChars: ['·'], stopChars: [' '] });
+                  var responseTexts = snippets.chunk(responseText, { len: 160, breakChars: ['·'], stopChars: [' '] });
 
                   for (var i = responseTexts.length-1; i>=0; i--) {
                     sendTextMessage(senderID, responseTexts[i]);

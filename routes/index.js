90a9116dd754a0336694df77f74ec5accf57779b
Simon Kofler <simon.kofler@hotmail.com>
2016-07-30 22:04:01 +0200

    Update project

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
   res.render('index', {
     pageTitle: res.__('JackTheBot - Free Amazon Price Tracking Bot.'),
     headerTitle: res.__('JackTheBot'),
-    headerSubtitle: res.__('The World\'s First Amazon Price Tracking Bot'),
+    headerSubtitle: res.__('The World\'s First Amazon Price Tracker Chatbot'),
     invitationToChat: res.__('We can now chat on Facebook Messenger! Wanna talk about the products you\'re interested on Amazon?'),
     invitationToSubscribe: res.__('Want me in your country? Tell me where you are and I\'ll get there as fast as I can.'),
     privacyLinkText: res.__('Privacy Policy'),
